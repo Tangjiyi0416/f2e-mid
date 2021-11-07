2d2e24b2bf0385a82c0667163000762ae7356805
Tang jiyi <37148354+Tangjiyi0416@users.noreply.github.com>
2021-11-08 04:39:51 +0800

    liver_detail completed

--- js/shared_layout.js
+++ js/shared_layout.js
@@ -2,11 +2,11 @@ $(function() {
     $("body").prepend(
         `
         <header>
-            <a class="header-link" href="index.html">首頁</a>
+            <a class="header-link" id="header-link-index" href="index.html">&nbsp&nbsp首頁&nbsp&nbsp</a>
             <div class="header-divider"></div>
-            <a class="header-link" href="liver_list.html">Liver一覽</a>
+            <a class="header-link" id="header-link-liver-list" href="liver_list.html">Liver一覽</a>
             <div class="header-divider"></div>
-            <a class="header-link" href="liver_picker.html">Liver選擇器</a>
+            <a class="header-link" id="header-link-liver-picker" href="liver_picker.html">Liver選擇器</a>
         </header>
         `
     );
