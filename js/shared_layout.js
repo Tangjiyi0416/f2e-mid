49a82b034103f454a431b4b676c09ca62c25c0a9
Tang jiyi <37148354+Tangjiyi0416@users.noreply.github.com>
2021-11-08 18:01:32 +0800

    liver-picker 90%

--- js/shared_layout.js
+++ js/shared_layout.js
@@ -2,11 +2,11 @@ $(function() {
     $("body").prepend(
         `
         <header>
-            <a class="header-link" id="header-link-index" href="index.html">&nbsp&nbsp首頁&nbsp&nbsp</a>
+            <a class="header-link" href="index.html"><span id="header-link-index">首頁</span></a>
             <div class="header-divider"></div>
-            <a class="header-link" id="header-link-liver-list" href="liver_list.html">Liver一覽</a>
+            <a class="header-link" href="liver_list.html"><span id="header-link-liver-list" >Liver一覽</span></a>
             <div class="header-divider"></div>
-            <a class="header-link" id="header-link-liver-picker" href="liver_picker.html">Liver選擇器</a>
+            <a class="header-link" href="liver_picker.html"><span  id="header-link-liver-picker">Liver選擇器</span></a>
         </header>
         `
     );
